348f6c6a65ccee6fd104aeea6f707b18dfb8de6d
xgg <fuzumin@outlook.com>
2021-01-31 22:35:40 +0800

    demo

---
+++ Demo/TS+React五子棋/wuziqi/src/components/CountCmp.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface Iprops {
+  num: number
+  handleChange:(num:number)=>void
+}
+
+const CountCmp:React.FC<Iprops> = ({ num, handleChange }) => {
+
+  const handleJian = (val:number) => {
+    handleChange(num + val)
+  }
+
+
+  return (
+    <div>
+      <button onClick={ () => handleJian(-1) }>-</button>
+      <p>{ num }</p>
+      <button onClick={ () => handleJian(1) }>+</button>
+    </div>
+  )
+}
+
+export default CountCmp;
