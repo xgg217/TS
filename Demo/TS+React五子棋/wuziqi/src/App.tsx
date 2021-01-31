e8117fca1674f5d9613dd75b6a2fbfb1eaa73ca0
xgg <fuzumin@outlook.com>
2021-01-31 22:32:46 +0800

    暂定初始化

--- Demo/TS+React五子棋/wuziqi/src/App.tsx
+++ Demo/TS+React五子棋/wuziqi/src/App.tsx
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
+import "./App.css"
+import BoardCmp from "./components/BoardCmp"
+import { ChessType } from './types/enums';
 
-function App() {
+
+const App:React.FC = () => {
+
+  const typeArr:ChessType[] = [
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+    ChessType.none,
+  ];
+
+  
+
   return (
-    <div>
-      12
+    <div className="box">
+      <BoardCmp chessesArr={ typeArr }></BoardCmp>
     </div>
-  );
+  )
 }
 
 export default App;
