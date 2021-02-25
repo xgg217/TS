330fbf438944aaa229b1551e541d4ae0f1456c35
fuzumin <fuzumin@outlook.com>
2021-02-25 14:53:33 +0800

    增加发牌功能

--- Demo/扑克牌/ts/src/tools/poker.ts
+++ Demo/扑克牌/ts/src/tools/poker.ts
@@ -86,6 +86,45 @@ class Poker {
    * 发牌结束后，存在4个数组
    * 
    */
+  licensing(){
+    const arr = this.setUserNmu(3, 3);
+
+    const newPokerArr = JSON.parse(JSON.stringify(this.pokerArr));
+    let ind = 0;
+    arr.forEach((item, index) => {
+      const name = "user" + (index + 1);
+      const newArr = newPokerArr.slice(ind, item);
+      ind = item;
+
+      this.licensingObj[name] = [...newArr];
+      
+    });
+
+    this.licensingObj.bot = newPokerArr.slice(arr[2], arr[2] + 3);
+  }
+
+  /**
+   * 辅助函数
+   * 设置每个玩家的牌的张数
+   * @param userLen 玩家的数量
+   * @param botLen 底牌的数量
+   */
+  private setUserNmu(userLen:number, botLen:number):number[] {
+    const len = this.pokerArr.length;
+    const num =  (len - botLen) / userLen;
+    const arr:number[] = [];
+
+    let ind = 0;
+    for (let i = 0; i < userLen; i++) {
+      if(i === 0) {
+        ind = ind + num;
+      } else  {
+        ind = ind + num;
+      }
+      arr.push(ind);
+    }
+    return arr;
+  }
 
 }
 
