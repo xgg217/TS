c6ce1abc53fb2b33366d62e1485682603b2f2a13
xgg <fuzumin@outlook.com>
2021-03-30 23:00:30 +0800

    测试

--- Demo/俄罗斯方块/src/index.ts
+++ Demo/俄罗斯方块/src/index.ts
@@ -1,12 +1,48 @@
-import { Square } from "./core/Square"
+import { Square } from "./core/Square";
+import { IView } from "./core/types"
+
+class SquareConsoleViewer implements IView {
+  private square: Square;
+  
+
+  constructor(square:Square) {
+    this.square = square;
+  }
+
+  /**
+   * 显示
+   */
+  show(): void {
+    console.log(this.square.point);
+    
+  }
+
+  /**
+   * 移除
+   */
+  remove(): void {
+    console.log(this.square);
+  }
+
+
+}
+
 
 const square = new Square({ x: 0, y:0}, "red");
 
+
+
+square.viewer = new SquareConsoleViewer(square);
+square.viewer.show();
+
 square.point = {
-  x: 1,
-  y: 1
+  x: 10,
+  y: 10
 }
 
-square.viewer
+square.point = {
+  x: 100,
+  y: 100
+}
 
 export {}
